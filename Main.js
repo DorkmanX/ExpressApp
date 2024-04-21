973a8321e5755683908f8ad9bd4cb66864c7e6d3
DorkmanX <kamil.rzezniczek@gmail.com>
2024-04-21 22:06:50 +0200

    Added mongodb local instance

--- Main.js
+++ Main.js
@@ -1,9 +1,33 @@
 const express = require('express')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/expressdb');
+
+const userSchema = new mongoose.Schema({ 
+    login: String, 
+    password: String, 
+    token: String,
+    name: String, 
+    surname: String, 
+    activated: Boolean,
+    resetPassword: Boolean
+ });
+const User = mongoose.model('User', userSchema,'Users');
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 2')
+    
+    const newUser = new User({ 
+        login: 'rzeznikx',
+        password: '1234abcd',
+        token: '',
+        name: 'Marek',
+        surname: 'Rzezniczek',
+        activated: false,
+        resetPassword: false
+    });
+    newUser.save().then(() => res.send('User added sucessfully!'));
 })
 
 app.listen(port, () => {
